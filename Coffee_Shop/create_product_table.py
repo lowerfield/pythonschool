e57aa563510862bf70de90582a9f86a0515487ae
lowerfield@gmail.com <manuelperez@manuels-mbp.config>
2015-03-02 21:44:34 +0000

    Improved table creation

--- Coffee_Shop/create_product_table.py
+++ Coffee_Shop/create_product_table.py
@@ -1,10 +1,29 @@
 import sqlite3
 
-def create_table(db_name,sql):
+def create_table(db_name,table_name,sql):
+
 	with sqlite3.connect(db_name) as db:
 		cursor = db.cursor()
-		cursor.execute(sql)
-		db.commit()
+		cursor.execute("select name from sqlite_master where name=?",(table_name,))
+		result = cursor.fetchall()
+		keep_table = True
+
+		if len(result) == 1:
+			response = input("The table {0} already exists, do you wish to recreate it ('y'/'n'): ".format(table_name))
+
+			if response == "y":
+				keep_table = False
+				print ("The {0} table will be recreated - all data will be lost".format(table_name))
+				cursor.execute("drop table if exists {0}".format(table_name))
+				db.commit()
+			else:
+				print "The existing table was kept"
+		else:
+			keep_table = False
+		if not keep_table:
+			cursor.execute(sql)
+			db.commit()
+
 if __name__ == "__main__":
 	db_name = "coffee_shop.db"
 	sql = """create table Product
@@ -12,4 +31,5 @@ if __name__ == "__main__":
 			Name text,
 			Price real,
 			primary key(ProductID))"""
-	create_table(db_name,sql)
+
+	create_table(db_name,"Product",sql)
